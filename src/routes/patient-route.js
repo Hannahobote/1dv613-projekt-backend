c8237518cfab83590698779e3650431082b5dc2a
Hannah Obote <ao223ir@student.lnu.se>
2023-07-31 19:01:56 +0200

    fix error

--- src/routes/patient-route.js
+++ src/routes/patient-route.js
@@ -10,7 +10,7 @@ const auth = new AuthController()
 router.post('/', auth.authorize, auth.userPremissionAdmin, (req, res, next) => patient.create(req, res, next))
 
 // Read one
-router.get('/:id', auth.authorize, auth.userPremissionAdmin, (req, res, next) => employee.readOne(req, res , next))
+router.get('/:id', auth.authorize, auth.userPremissionAdmin, (req, res, next) => patient.readOne(req, res , next))
 
 // read all patients
 router.get('/', auth.authorize, auth.userPremissionAdmin, (req, res, next) => patient.readAll(req, res , next))
