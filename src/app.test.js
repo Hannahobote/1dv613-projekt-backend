a2700b5b4c4e283dc7f4ff2524f6ad20e47b78c6
Hannah Obote <ao223ir@student.lnu.se>
2023-11-29 23:40:44 +0100

    test cases

--- src/app.test.js
+++ src/app.test.js
@@ -1,13 +1,18 @@
 import request from 'supertest';
 import { app } from "./server";
-import {describe, expect, test, it } from '@jest/globals';
+import { describe, expect, test, it } from '@jest/globals';
+import dotenv from 'dotenv'
+dotenv.config()
 
-describe('Auth API', () => { 
+let bearerToken = ''
+
+describe('Auth API', () => {
   test(' POST /api/auth/login -> Login a user', async () => {
     const response = await request(app)
-    .post('/api/auth/login')
-    .send({ username: 'chef', password: 'password' });
+      .post('/api/auth/login')
+      .send({ username: 'chef', password: process.env.PASSWORD });
     expect(response.status).toBe(200);
+    bearerToken = response.body.accessToken
     expect(response.body).toHaveProperty('msg')
     expect(response.body).toHaveProperty('accessToken')
     expect(response.body).toHaveProperty('user')
@@ -16,46 +21,87 @@ describe('Auth API', () => {
 
   test('POST api/auth -> Should give 404 if user doesnt exist', async () => {
     const response = await request(app)
-    .post('/api/auth/login')
-    .send({ username: 'lördagsGodis', password: '1234567' });
+      .post('/api/auth/login')
+      .send({ username: 'lördagsGodis', password: '1234567' });
     expect(response.status).toBe(404);
   })
 
-  test('POST /api/auth -> Should give 401 if credentials are incorrect ',async () => {
+  test('POST /api/auth -> Should give 401 if credentials are incorrect ', async () => {
     const response = await request(app)
-    .post('/api/auth/login')
-    .send({ username: 'chef', password: '1234567' });
+      .post('/api/auth/login')
+      .send({ username: 'chef', password: '1234567' });
     expect(response.status).toBe(401);
   })
 })
 
 
 describe('Employee API', () => {
-  test('GET /api/employee -> should return a list of all employees', async() => {
-    request(app)
-      .get('api/employee')
-      .expect(200)
+  test('GET /api/employee -> should return a list of all employees', async () => {
+    const response = await request(app)
+      .get('/api/employee')
+      .set('Authorization', `Bearer ${bearerToken}`)
+    expect(response.status).toBe(200);
   })
 
-  test('GET /api/employee/:id -> should return one employee', async () => {
+  test('GET /api/employee/:id -> should return one employee (this should fail if user is not signed in)', async () => {
+    const response = await request(app)
+      .get('api/employee/6446cb61dc92030c315b5744')
+      .set('Authorization', `Bearer ${bearerToken}`)
+    expect(response.status).toBe(200);
 
+    expect(response.body).toEqual(expect.objectContaining({
+      _id: expect.any(String),
+      username: expect.any(String),
+      password: expect.any(String),
+      name: expect.any(String),
+      surname: expect.any(String),
+      personnr: expect.any(String),
+      worknr: expect.any(Number),
+      role: expect.any(String),
+      createdAt: expect.any(Date),
+      updatedAt: expect.any(Date),
+    }))
   })
 
-  
+
   test('GET /api/employee/:id -> should return 404 if employee does not exist', async () => {
- 
+    const response = await request(app)
+      .get('api/employee/6446cb61dc92030c315b5748')
+      .set('Authorization', `Bearer ${bearerToken}`)
+    expect(response.status).toBe(404);
   })
 
-  
-  
-  test('POST /api/employee -> should return create an emoloyee', async () => {
- 
+
+  // mock database for this to work
+  test('POST /api/employee -> should return 201 + new employee details when employee has been created', async () => {
+
+
+
   })
 
-  
-  
-  test('GET /api/employee/:id -> should return 409 if we try to create employee with the same work number', async () => {
- 
+  // mock database for this to work
+  test('GET /api/employee -> should return 409 if we try to create employee with the same work number, personr, username', async () => {
+
+  })
+
+  // mock database for this to work
+  test('PUT /api/employee/:id -> should return 200 + updated employee when member has been updated', async () => {
+
+  })
+
+  // mock database for this to work
+  test('PUT /api/employee/:id -> should return 404 of we try to update a member that does not exist', async () => {
+
+  })
+
+  // mock database for this to work
+  test('DELETE /api/employee/:id -> should return 204 when employee has been deleted', async () => {
+
+  })
+
+  // mock database for this to work
+  test('DELETE /api/employee/:id -> should return 404 if employee does not exist', async () => {
+
   })
 
 })
